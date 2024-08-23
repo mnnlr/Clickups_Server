427a28ce6e514f1f430e083f46d87f3b3bfca731
RUSHI PANCHAL <mern.mnnlr.rushipanchal@gmail.com>
2024-08-23 19:21:39 +0530

    updated index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDB = require('./connectDB');
 require('dotenv').config();
 
 const app = express();
@@ -14,11 +14,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-
-mongoose.connect(process.env.mongoDb).then(() => {
-    console.log("MongoDB connected.");
-})
-
+connectDB();
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
