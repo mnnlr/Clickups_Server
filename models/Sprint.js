56405ee689ac5f9991189468a7c862fe8f0689f9
RUSHI PANCHAL <mern.mnnlr.rushipanchal@gmail.com>
2024-08-23 20:03:27 +0530

    added team and comment schemas

--- models/Sprint.js
+++ models/Sprint.js
@@ -1,10 +1,19 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const sprintSchema=new mongoose.Schema({
-    projectId:{type:mongoose.Schema.ObjectId,ref:"Project"},
-    task:{type:mongoose.Schema.ObjectId,ref:"Task"},
-    startDate:{type:Date,default:Date.now},
-    endDate:{type:Date}
+const sprintSchema = new mongoose.Schema({
+    projectId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Project"
+    },
+    task: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Task"
+    },
+    startDate: {
+        type: Date,
+        default: Date.now
+    },
+    endDate: { type: Date }
 })
-const Sprint =mongoose.model("Sprint",sprintSchema)
-module.exports=Sprint
+const Sprint = mongoose.model("Sprint", sprintSchema)
+module.exports = Sprint
--- models/User.js
+++ models/User.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
-  user: { type: String, required: true },
-  password: { type: String, required: true },
-  role:{type:String,required:true,enum:["User","Amdin"]},
-  email: { type: String, required: true },
-  timestamp:{type:Date,default:Date.now}
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["User", "Admin"]
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const User = mongoose.model('User', UserSchema);
