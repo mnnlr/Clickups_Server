687b87ea8c633a0734121b9272c7b5f3cf7d59f4
Rohit Kumar <rpal46515@gmail.com>
2024-08-27 14:22:09 +0530

    Update commentRoutes.js

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,18 +2,12 @@ const express = require('express');
 const commentRouter = express.Router();
 const CommentController = require('../controllers/commentController');
 
-<<<<<<< HEAD
-const router = express.Router();
-
-router.route('/')
-=======
 commentRouter.route('/')
->>>>>>> 8fb122f66bce2a6c21542a8d5728302f2624e99c
 .post(CommentController.createComment);
 
 commentRouter.route('/:id')
 .patch(CommentController.updateCommentById)
 .delete(CommentController.deleteCommentById);
-commentRouter.get('/',);
+
 
 module.exports = commentRouter; 
