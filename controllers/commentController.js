126a775eb2e44fd4aec7eec2c9fbe39d7fd7c49c
Rohit Kumar <rpal46515@gmail.com>
2024-08-27 11:22:23 +0530

    Add files via upload

---
+++ controllers/commentController.js
@@ -0,0 +1,48 @@
+const Comments = require('../models/comment')
+
+
+//create comment
+exports.createComment = async (req, res) => {
+    try {
+        const { taskId, creatorId, comment } = req.body;
+        const newComment = new Comments({
+            taskId,
+            creatorId,
+            comment
+        })
+        const saveComment = await newComment.save();
+        res.status(200).json({ message: "Comment Created Successfully", success: true })
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false })
+    }
+
+}
+
+// update comment by id
+exports.updateCommentById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { comment } = req.body;
+        const updateComment = await Comments.findByIdAndUpdate(id, { comment }, { new: true })
+        if (!updateComment) {
+            return res.status(404).json({ message: "Comment Not Found", success: false })
+        }
+        res.status(200).json({ message: "Comment Updated Successfully", success: true })
+    } catch (error) {
+        res.status(500).json({ message: error.message, duccess: true })
+    }
+}
+
+//delete Comment BY id
+exports.deleteCommentById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteComment = await Comments.findByIdAndDelete(id);
+        if (!deleteComment) {
+            return res.status(404).json({ message: "Comment Not Found", success: false })
+        }
+        res.status(200).json({ message: "Delete Comment Successfully", success: true })
+    } catch (error) {
+        res.status(500).json({ message: error.message, success: false })
+    }
+}
