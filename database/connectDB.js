e7d0eaed1e54bd0612ab3304b072f81f1365a5d0
Yash Maurya <yashmaurya2109@gmail.com>
2024-08-28 15:12:06 +0530

    Changed name of env

--- database/connectDB.js
+++ database/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const data = await mongoose.connect(process.env.MONGO_DB_URL);
+        const data = await mongoose.connect(process.env.MONGO_URL);
         console.log(`Mongodb connected with server : ${data.connection.host}`);
     } catch (err) {
         console.log(`Error while connecting with database`, err.message);
