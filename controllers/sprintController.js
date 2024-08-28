c11fbb6216f9fb79ceb2b492a119abd865328bd2
Yash Maurya <yashmaurya2109@gmail.com>
2024-08-28 10:40:36 +0530

    Added sprint

--- controllers/sprintController.js
+++ controllers/sprintController.js
@@ -1,27 +1,62 @@
-const Sprint = require('../models/Sprint')
-const Project = require('../models/Project')
-const Task = require('../models/Task')
+const Sprint = require('../models/Sprint');
+const Project = require('../models/Project');
+const Task = require('../models/Task');
 
+// Create a new sprint
 const createSprint = async (req, res) => {
-    const {projectId, task, startDate, endDate} = req.body
-    try{
-       const isTask = await Task.findById(task)
-     if(!isTask) return res.status(404).json({message: 'Task not found'})
-       const isProject = await Project.findById(projectId)
-       if(!isProject) return res.status(404).json({message: 'Project not found'})
-       const newSprint = new Sprint({
-        projectId,
-        task,
-        startDate,
-        endDate
-       })
-       await newSprint.save()
-       return res.status(201).json(newSprint)
-    }catch(err){
-        res.status(500).json({message: err.message})
+    const { projectId, task, startDate, endDate } = req.body;
+    try {
+        const isTask = await Task.findById(task);
+        if (!isTask) return res.status(404).json({ message: 'Task not found' });
+
+        const isProject = await Project.findById(projectId);
+        if (!isProject) return res.status(404).json({ message: 'Project not found' });
+
+        const newSprint = new Sprint({
+            projectId,
+            task,
+            startDate,
+            endDate
+        });
+        await newSprint.save();
+        
+isProject.sprintId.push(newSprint._id);
+        await isProject.save()
+
+        return res.status(201).json({ message: "Sprint created successfully", succcess: true });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-}
+};
+
+
+const getSprints = async (req, res) => {
+    try {
+        const sprints = await Sprint.find();
+        return res.status(200).json({ message: "Sprint get successfully", Data: sprints });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+
+const deleteSprint = async (req, res) => {
+    const { sprintId } = req.params; 
+    try {
+        const isSprint = await Sprint.findById(sprintId);
+        if (!isSprint) return res.status(404).json({ message: 'Sprint not found' });
+
+        const deletedSprint = await Sprint.findByIdAndDelete(sprintId);
+        res.status(200).json({ message: 'Sprint deleted successfully', succcess: true});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 module.exports = {
-    createSprint
-}
+    createSprint,
+    getSprints,
+
+    deleteSprint
+};
