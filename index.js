047395b4313076e95aaed157938eb6978feb3e0d
mern-mnnlr-rushi <mern.mnnlr.rushipanchal@gmail.com>
2024-12-11 21:28:24 +0530

    added custom payload size limit

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
+// payload size limit
+app.use(express.json({ limit: '500kb' })); // Increase payload limit
+app.use(express.urlencoded({ extended: true, limit: '500kb' })); // For URL-encoded data
 app.use(express.json());
-
 app.use(refreshTokenRoute);
 
 app.use("/api/tasks", taskRoutes);
