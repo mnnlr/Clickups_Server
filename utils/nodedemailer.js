fc19eb336a4ff817ce02095c2a46c6e66fa3d12a
MNNLRAbhijitMaske <mern.mnnlr.abhijitmaske@gmail.com>
2025-01-22 16:51:48 +0530

    change nodemailer credentials

--- utils/nodedemailer.js
+++ utils/nodedemailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
 export const transporter=nodemailer.createTransport({
     service:"Gmail",
     auth:{
-      user:process.env.EMAIL,
-      pass:process.env.PASSWORD,
+      user:process.env.SMTP_EMAIL,
+      pass:process.env.SMTP_PASSWORD,
     }
   });
